e3baa6549bffdd6159af43ec5e82c358dedf80d7
unknown <teona.piranishvili1@gmail.com>
2023-04-26 14:15:53 +0400

    feat: add order product functionality

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ function getCommand() {
       const purchasePrice = parseInt(args[3]);
       purchaseProduct(purchaseProductId, purchaseQuantity, purchasePrice);
       return `Product purchased and balance updated in catalog.json: {product_id: "${purchaseProductId}", purchase_quantity: "${purchaseQuantity}", purchase_price: "${purchasePrice}"}`;
+    case "order_product":
+      const orderProductId = args[1];
+      const orderQuantity = parseInt(args[2]);
+      orderProduct(orderProductId, orderQuantity);
+      return `Order placed and balance updated in catalog.json: { product_id: "${orderProductId}", order_quantity: "${orderQuantity}"}`;
     default:
       return `Invalid command: ${command}`;
   }
@@ -49,5 +54,25 @@ function purchaseProduct(productId, quantity, price) {
   catalog[productIndex] = product;
   fs.writeFileSync("catalog.json", JSON.stringify(catalog));
 }
-
+function orderProduct(productId, quantity) {
+  const catalog = JSON.parse(fs.readFileSync("catalog.json", "utf8"));
+  const productIndex = catalog.findIndex(
+    (product) => product.product_id === productId
+  );
+  if (productIndex === -1) {
+    console.log(`Product not found: ${productId}`);
+    return;
+  }
+  const product = catalog[productIndex];
+  if (product.balance < quantity) {
+    console.log(`Insufficient stock for product ${productId}`);
+    return;
+  }
+  product.balance -= quantity;
+  catalog[productIndex] = product;
+  const orders = JSON.parse(fs.readFileSync("orders.json", "utf8"));
+  orders.push({ product_id: productId, quantity: quantity });
+  fs.writeFileSync("catalog.json", JSON.stringify(catalog));
+  fs.writeFileSync("orders.json", JSON.stringify(orders));
+}
 console.log(getCommand());
